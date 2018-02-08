9095c36ed5cc9c61385b03463795a15976669499
mrApptastic <henrikbeske@gmail.com>
2018-02-08 15:25:40 +0100

    Added mrSelect and mrHappy

--- mrMr.js
+++ mrMr.js
@@ -60,7 +60,121 @@ var mrMr = angular.module('mrMr', []);
 				s: "@"
 			},			
         }
-    });		
+    });
+
+    mrMr.directive("mrSelect", function () {
+        return {
+            restrict: "E",
+            template: '<span style="position: relative;">' +
+            '<input type="text" class="apSelect" style="width:{{width}}; height:{{height}}px; border:{{borderStyle}}; border-radius: 4px; border-color: rgb(169, 169, 169); background-color: white; cursor: default; border-width: 1px; align-items: center; white-space: pre; padding: 0 0 0 4px; margin: 0 0 0 0; padding-left: {{textPadding}}px;" placeholder="{{text}}" ng-click="setFocus()" ng-change="textChanged()" ng-model="searchText" ng-model-options="{ debounce: 500 }" / >' +
+            '<span class="apSelectClick" ng-click="showSelect = !showSelect" style="height:{{height - 1}}px;margin-top: {{tabOffset}}px; position: absolute; right: 1px; top: -2px; background: lightgrey; z-index: 1; padding: 0px 5px 0px 0px; border-radius: 0 4px 4px 0; font-weight: bold; cursor: pointer;padding-right: {{tabWidth}}px;">' +
+            '&nbsp<i style="' +
+            'font-size: 16px;position:absolute;left: calc(50% - {{load ? \'10\' : \'4.5\'}}px); top: calc(50% - 8px); position: absolute;"' +
+            ' class="text-muted fa" ng-class="load ? \'fa-spinner fa-pulse fa-3x fa-fw\' : (showSelect ? \'fa-caret-up\' : \'fa-caret-down\') "' +
+            ' aria-hidden="true"></i>' +
+            '</span>' +
+            '<ul class="apSelectItems" ng-show="showSelect" style="width:{{width}}; position: absolute; z-index: 99; left: 0; margin-left: 0px;">' +
+            '' +
+            '<li ng-mouseenter="tipIn($event)" ng-mouseleave="tipOut($event)" ng-repeat="n in data | orderBy : [p] : false" style="width:{{width}}; border: {{borderStyle}}; {{$index == data.length -1 ? \'\' : \'border-bottom: none;\'}}; list-style: none; margin-left: -40px; padding: 0; left: 0; top: 0; background: white; padding-left: 6px; z-index: 150; cursor: pointer;" ng-click="selected(n)">{{n[p]}}</li>' +
+            '</ul>' +
+            '</span>',
+            scope: {
+                p: "@",		/* Key: Object key for select box. */
+                m: "@",		/* ng-model in parent scope. */
+                u: "@",		/* Update function in parent scope. */
+                b: "@",     /* Broadcast param to update data from parent. E.g. $scope.$broadcast('{BROADCAST PARAM}', $scope.data); */
+                e: "@",     /* Broadcast param to send message that an update error occured in parent. */
+                d: "@",     /* Call function in parent scope when ng-model changes (Optional) */
+                t: "@",		/* Placeholder text (Optional). */
+                w: "@",		/* Width: Width of select box (Optional). */
+                h: "@",		/* Height: Height in pixels of select box (Optional). */
+                s: "@",     /* Tab Width: Width of tab (Optional). */
+                l: "@",     /* Text Left Offset: Left padding for text and placeholder in pixels (Optional). */
+                z: "@"      /* Border Style: CSS Input for borders (Optional). */
+            },
+            link: function (scope) {
+                /* Data for this select box. Refreshed on broadcast from parent scope */
+                scope.data = [];
+                /* Search text for select box. Initialised as empty */
+                scope.searchText = "";
+                /* Width. Defaults to 150px */
+                scope.width = scope.w != null && scope.w.length > 0 ? scope.w : '150px';
+                /* Height. Defaults to 22px */
+                scope.height = scope.h != null && scope.h.length > 0 ? scope.h : '22';
+                /* Placeholder text. Defaults to empty string */
+                scope.text = scope.t != null && scope.t.length > 0 ? scope.t : "";
+                /* Boolean to handle spinner when loading */
+                scope.load = false;
+                /* Calculate Offset for tab depending on height */
+                scope.tabOffset = -(0.5 * parseInt(scope.height)) + 11;
+                /* Width of tab */
+                scope.tabWidth = scope.s != null && scope.s.length > 0 ? scope.s : '20';
+                /* Text Padding Left */
+                scope.textPadding = scope.l != null && scope.l.length > 0 ? scope.l : '12';
+                /* Border Style */
+                scope.borderStyle = scope.z != null && scope.z.length > 0 ? scope.z : '1px solid #ccc';
+                /* Unset selected object and open select and launch request in parent scope for new data when search text is changed */
+                scope.textChanged = function () {
+                    scope.load = true;
+                    scope.$parent[scope.m] = null;
+                    scope.showSelect = true;
+                    scope.$parent[scope.u](scope.searchText.toLowerCase());
+                };
+                /* Select unset focus depending on whether select box is open or not */
+                scope.setFocus = function () {
+                    if (scope.showSelect) {
+                        angular.element(document.activeElement).blur();
+                        scope.load = false;
+                    }
+                    scope.showSelect = !scope.showSelect;
+                };
+                /* When selected object is chosen the text is updated and the box is collaped */
+                scope.selected = function (model) {
+                    scope.searchText = model[scope.p];
+                    scope.$parent[scope.m] = model;
+                    if (scope.d) {
+                        scope.$parent[scope.d](model);
+                    }
+                    scope.showSelect = false;
+                };
+                scope.tipIn = function ($event) {
+                    $event.target.style.color = "snow";
+                    $event.target.style.background = "dodgerblue";
+                };
+                scope.tipOut = function ($event) {
+                    $event.target.style.color = "initial";
+                    $event.target.style.background = "white";
+                };
+                /* Listen to parent scope for broadcast */
+                scope.$on(scope.b.toString(), function (event, data) {
+                    /* Refresh data from parent */
+                    scope.data = data;
+                    scope.load = false;
+                    /* If data has only one object, then this must be right one and is set as the selected one and box is collapsed */
+                    if (scope.data.length == 1) {
+                        scope.searchText = scope.data[0][scope.p];
+                        scope.$parent[scope.m] = scope.data[0];
+                        if (scope.d) {
+                            scope.$parent[scope.d](scope.data[0]);
+                        }
+                        scope.showSelect = false;
+                    }
+                });
+                if (scope.e) {
+                    scope.$on(scope.e.toString(), function (event, data, message) {
+                        /* An error has occured in update from parent scope */
+                        scope.load = false;
+                        console.log("An error occured when attempting to update data from parent scope" + (message ? (": " + message + ".") : "."));
+                        if (scope.data) {
+                            scope.data = data;
+                            scope.$parent[scope.m] = scope.data[0];
+                            scope.showSelect = false;
+                        }
+                    });
+                }
+            }
+        }
+    });
 	
     mrMr.filter('mrHtml', ['$sce', function ($sce) {
         return function (text) {
@@ -72,6 +186,22 @@ var mrMr = angular.module('mrMr', []);
         return function (text) {
 			var strippedText = text.replace(/<(?:.|\n)*?>/gm, '');
             return strippedText;
+        };
+    }]);
+
+    mrMr.filter('mrHappy', ['$sce', function ($sce) {
+        return function (text, cls) {
+            var colours = cls ? cls : ["Red", "Blue", "Orange", "Purple", "Green"];
+            var returnText = "";
+            var colourIndex = 0;
+            for (let i = 0; i < text.length; i++) {
+                returnText += "<span style='color:" + colours[colourIndex] + ";'>" + text.charAt(i) + "</span>";
+                colourIndex++;
+                if (colourIndex == colours.length) {
+                    colourIndex = 0;
+                }
+            }
+            return $sce.trustAsHtml(returnText);
         };
     }]);
 	
